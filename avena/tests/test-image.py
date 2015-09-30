43b00bdb18131c49a6e52d752aeb0549298d8cda
Mansour Moufid <mansourmoufid@gmail.com>
2015-09-30 14:05:16 -0400

    Add more unit tests for the image module.

--- avena/tests/test-image.py
+++ avena/tests/test-image.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-from numpy import all, array, dstack
+from numpy import all, allclose, array, dstack
+from os import remove
+from os.path import sep, split
 
-from .. import image
+from .. import image, utils
 
 
 def test_get_channels():
@@ -23,6 +25,18 @@ def test_map_to_channels():
         y,
     )
     assert all(z == y + 1)
+
+
+def test_read_save():
+    f = split(__file__)[0] + sep + 'drink.png'
+    x = image.read(f)
+    tmp = utils.rand_filename(f)
+    try:
+        image.save(x, tmp)
+        y = image.read(tmp)
+        assert allclose(x, y, rtol=1e-4, atol=1e-1)
+    finally:
+        remove(tmp)
 
 
 if __name__ == '__main__':
