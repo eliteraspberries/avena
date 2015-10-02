762ebc5724593aa532f88b22d66f55422af1a869
Mansour Moufid <mansourmoufid@gmail.com>
2015-10-02 12:39:55 -0400

    Refactor index calculations in the interp module.

--- avena/interp.py
+++ avena/interp.py
@@ -5,6 +5,7 @@
 
 from functools import partial
 from numpy import (
+    ceil as _ceil,
     complex64 as _complex64,
     multiply as _multiply,
     real as _real,
@@ -29,7 +30,7 @@ def _interp2_shape(factor, shape):
 
 def _mirror(array):
     m, n = array.shape
-    r = int(_sqrt((m // 2) ** 2 + (n // 2) ** 2))
+    r = int(_ceil(_sqrt((m / 2.0) ** 2 + (n / 2.0) ** 2)))
     x = tile._tile9_periodic(array)
     f = filter._low_pass_filter(x.shape, r)
     _multiply(x, f, out=x)
@@ -51,7 +52,7 @@ def _interp2(factor, array):
     z = _ifft2(Z, s=(p, q))
     z = _real(z)
     m, n = _interp2_shape(factor, array.shape)
-    i, j = (p - m) // 2, (q - n) // 2
+    i, j = int(_ceil((p - m) / 2.0)), int(_ceil((q - n) / 2.0))
     z = z[i:(i + m), j:(j + n)]
     return z
 
