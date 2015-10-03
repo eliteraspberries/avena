71cd470f727c61330d22fda8b9a9f94e47b6353f
Mansour Moufid <mansourmoufid@gmail.com>
2015-10-03 17:53:36 -0400

    Add a unit test for the logistic module.

---
+++ avena/tests/test-logistic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from numpy import all, array, random
+
+from .. import logistic
+
+
+def test_logistic():
+    x = random.random_sample(100)
+    x *= 10.0
+    x -= 5.0
+    for k in [1.0, 2.0, 5.0, 10.0]:
+        y = logistic._logistic(k, x)
+        assert all(y >= 0.0) and all(y <= 1.0)
+
+
+if __name__ == '__main__':
+    pass
