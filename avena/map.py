052953d74721273309f3310849911b3f6bc4c09f
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-19 19:08:44 -0500

    Add a tile mapping function.

--- avena/map.py
+++ avena/map.py
@@ -5,7 +5,7 @@
 
 import numpy
 
-from . import image
+from . import image, np
 
 
 def map_to_channels(func, img):
@@ -16,6 +16,35 @@ def map_to_channels(func, img):
     for channel in channels:
         z = numpy.dstack((z, func(channel)))
     return z
+
+
+def _num_tiles(shape, tile_shape):
+    m, n = shape
+    a, b = tile_shape
+    p = int(numpy.ceil(float(m) / a))
+    q = int(numpy.ceil(float(n) / b))
+    return (p, q)
+
+
+def _tile_view(tile_shape, array, coords):
+    a, b = tile_shape
+    i, j = coords
+    return array[(i * a):(i * a + a), (j * b):(j * b + b)]
+
+
+def _map_to_tiles(tile_shape, func, array):
+    a, b = tile_shape
+    m, n = _num_tiles(array.shape, tile_shape)
+    array = np._zeropad(array, (m * a, n * b))
+    result = func(array[:a, :b])
+    c, d = result.shape
+    result = np._zeropad(result, (m * c, n * d))
+    indices = ((i, j) for i in range(m) for j in range(n))
+    for (i, j) in indices:
+        x = _tile_view((a, b), array, (i, j))
+        z = _tile_view((c, d), result, (i, j))
+        z[:, :] = func(x)
+    return result
 
 
 if __name__ == '__main__':
--- avena/tests/test-map.py
+++ avena/tests/test-map.py
@@ -14,5 +14,30 @@ def test_map_to_channels():
     assert numpy.all(z == y + 1)
 
 
+def test_num_tiles():
+    assert map._num_tiles((1, 1), (1, 1)) == (1, 1)
+    assert map._num_tiles((1, 2), (1, 1)) == (1, 2)
+
+
+def test_tile_view():
+    x = numpy.array([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
+    y = numpy.array([[1, 2, 3]])
+    z = map._tile_view((1, 3), x, (0, 0))
+    assert numpy.all(z == y)
+    y = numpy.array([[2, 3, 4]])
+    z = map._tile_view((1, 3), x, (1, 0))
+    assert numpy.all(z == y)
+
+
+def test_map_to_tiles():
+    x = numpy.array([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
+    def f(a):
+        return a + 1
+    for shape in [(1, 1), (1, 3)]:
+        y = numpy.array([[2, 3, 4], [3, 4, 5], [4, 5, 6]])
+        z = map._map_to_tiles(shape, f, x)
+        assert numpy.all(z == y)
+
+
 if __name__ == '__main__':
     pass
